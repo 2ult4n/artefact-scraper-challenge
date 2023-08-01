fe42ccd75fcfa06b8135882f61c9eb23032055f4
2ult4n <isultanfx@gmail.com>
2023-08-01 22:51:12 +0300

    Added: Configuring mongodb pipeline along with its settings

---
+++ guardian_scraper/guardian_scraper/pipelines.py
@@ -0,0 +1,19 @@
+
+from pymongo import MongoClient
+from scrapy.exceptions import DropItem
+import os 
+
+class MongoDBPipeline(object):
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(mongo_conn=crawler.settings.get('MONGODB_CONNECTION'))
+
+    def __init__(self, mongo_conn):
+        client = MongoClient(mongo_conn)
+        db = client['guardian-data']
+        self.collection = db.news
+
+    def process_item(self, item, spider):
+        self.collection.insert_one(item)
+        return item
