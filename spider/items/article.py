e53e26486d4176afb38715ce12302700c3ad1459
2ult4n <isultanfx@gmail.com>
2023-08-01 22:35:00 +0300

    Deleted: Re-generate the porject to configure pipelines, middlewares etc

--- spider/items/article.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class Article(scrapy.Item):
-    author = scrapy.Field()
-    headline = scrapy.Field()
-    content = scrapy.Field()
-    url = scrapy.Field()
-    published_at = scrapy.Field()
